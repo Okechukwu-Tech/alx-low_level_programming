352d419c4f3ebce2a83ec5892a29355b244ea002
Okechukwu-tech <saviouroyibo7@gmail.com>
2023-02-19 15:39:03 +0100

    updated

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 
-/*
- * Main - entry point
- *
- * Return : always zero
- *
- */
+/**
+* main - Entry
+*
+* Return: Always 0
+*/
 
-int main()
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
